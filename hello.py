72a2b6f0227bd56609eff3eb7947027e3539e805
stefan <stefan.holmberg@systementor.se>
2024-05-06 19:15:40 +0200

    Added some files

---
+++ hello.py
@@ -0,0 +1,15 @@
+# local directory into a repository
+# git init
+
+# git add
+# vi skriver oftast git add .
+# den lägger till alla otrackade filer -> git repositoryt
+
+# git clone
+# git remote add origin
+# git commit
+# git push
+# git branch
+# git commit
+# git revert
+
